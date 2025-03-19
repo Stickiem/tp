3eab1a07335b0e939d725a2013387ca66dd1c4be
TrKNguyen <nguyenusaco@gmail.com>
2025-03-19 18:51:11 +0800

    Add find by email command

---
+++ src/main/java/seedu/address/logic/commands/FindEmailCommand.java
@@ -0,0 +1,57 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.predicate.EmailContainsKeywordsAsSubstringPredicate;
+
+/**
+ * Finds and lists all persons in address book whose email contains any of the argument keywords.
+ * Keyword matching is case-insensitive.
+ */
+public class FindEmailCommand extends Command {
+
+    public static final String COMMAND_WORD = "findemail";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose email contains any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " example1@example.com example2@example.com";
+
+    private final EmailContainsKeywordsAsSubstringPredicate predicate;
+
+    public FindEmailCommand(EmailContainsKeywordsAsSubstringPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof FindEmailCommand)) {
+            return false;
+        }
+
+        FindEmailCommand otherFindEmailCommand = (FindEmailCommand) other;
+        return predicate.equals(otherFindEmailCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
+}
---
+++ src/main/java/seedu/address/model/predicate/EmailContainsKeywordsAsSubstringPredicate.java
@@ -0,0 +1,45 @@
+package seedu.address.model.predicate;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.person.Person;
+
+public class EmailContainsKeywordsAsSubstringPredicate implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public EmailContainsKeywordsAsSubstringPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return keywords.stream()
+                .anyMatch(keyword -> containsSubstringIgnoreCase(person.getEmail().value, keyword));
+    }
+
+    private boolean containsSubstringIgnoreCase(String email, String keyword) {
+        return email.toLowerCase().contains(keyword.toLowerCase());
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof EmailContainsKeywordsAsSubstringPredicate)) {
+            return false;
+        }
+
+        EmailContainsKeywordsAsSubstringPredicate otherEmailContainsKeywordsAsSubstringPredicate
+                = (EmailContainsKeywordsAsSubstringPredicate) other;
+        return keywords.equals(otherEmailContainsKeywordsAsSubstringPredicate.keywords);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("keywords", keywords).toString();
+    }
+}
