1bfa20e797a3a06c67e79ec7125bfa33f93d7d95
TrKNguyen <nguyenusaco@gmail.com>
2025-04-02 00:54:28 +0800

    Update Redo Command

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -1,13 +1,10 @@
 package seedu.address.logic.commands;
 
-import java.util.Deque;
-import java.util.LinkedList;
-
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.AddressBookParser;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
-
+import seedu.address.model.commandhistory.CommandHistory;
 
 /**
  * Redo command that re-executes the x-th last command.
@@ -21,11 +18,7 @@ public class RedoCommand extends Command {
 
     public static final String MESSAGE_INVALID_COMMAND_INDEX = "Invalid command index. Must be between 1 and 5.";
     public static final String MESSAGE_NOT_ENOUGH_COMMAND_HISTORY = "Not enough commands in history to redo.";
-    /**
-     * Deque to store the last 5 user inputs.
-     * This will store the history of executed commands.
-     */
-    private static final Deque<String> lastCommands = new LinkedList<>();
+
     private final int commandIndex;
 
     public RedoCommand(int commandIndex) {
@@ -40,56 +33,19 @@ public class RedoCommand extends Command {
 
         String commandToRedo;
         try {
-            commandToRedo = getCommandFromHistory(commandIndex);
+            commandToRedo = CommandHistory.getCommandFromHistory(commandIndex);
         } catch (IndexOutOfBoundsException e) {
             throw new CommandException(MESSAGE_NOT_ENOUGH_COMMAND_HISTORY);
         }
 
         AddressBookParser parser = new AddressBookParser();
+        Command command;
         try {
-            parser.parseCommand(commandToRedo, model);
+            command = parser.parseCommand(commandToRedo, model);
         } catch (ParseException e) {
             throw new CommandException("Error while re-executing the command: " + commandToRedo);
         }
 
-        return new CommandResult("Re-executed command: " + commandToRedo);
-    }
-
-    /**
-     * Adds the given command input to the history of last 5 commands.
-     * If the list exceeds 5, removes the oldest command.
-     *
-     * @param userInput The command to add to the history
-     */
-    public static void addCommandToHistory(String userInput) {
-        if (lastCommands.size() == 5) {
-            lastCommands.poll();
-        }
-        lastCommands.offer(userInput);
-    }
-
-    /**
-     * Retrieves the command at the specified index from the lastCommands deque.
-     *
-     * @param index The index of the command in the deque (1-based).
-     * @return The command string at the specified index.
-     */
-    private String getCommandFromHistory(int index) {
-        int dequeIndex = 5 - index;
-
-        if (lastCommands.size() <= dequeIndex) {
-            throw new IndexOutOfBoundsException("Not enough commands in history to redo.");
-        }
-
-        return (String) lastCommands.toArray()[dequeIndex];
-    }
-
-    /**
-     * Returns the last 5 commands (user inputs).
-     *
-     * @return list of last 5 commands.
-     */
-    public static Deque<String> getLastCommands() {
-        return lastCommands;
+        return command.execute(model);
     }
 }
