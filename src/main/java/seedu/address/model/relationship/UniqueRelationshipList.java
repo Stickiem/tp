5009d4dab8f7577d80db0ad3a0fcbb9d38e3bc45
itsvari <13640661+itsvari@users.noreply.github.com>
2025-04-03 20:01:03 +0800

    Remove unused imports

--- src/main/java/seedu/address/model/relationship/UniqueRelationshipList.java
+++ src/main/java/seedu/address/model/relationship/UniqueRelationshipList.java
@@ -4,9 +4,7 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
