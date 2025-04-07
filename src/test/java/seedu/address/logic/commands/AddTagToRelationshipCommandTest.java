e05a8243afb3e1d5a31b8f1bcfa1262dcd74c40f
itsvari <13640661+itsvari@users.noreply.github.com>
2025-04-08 04:13:16 +0800

    Add more tests

--- src/test/java/seedu/address/logic/commands/AddTagToRelationshipCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTagToRelationshipCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
@@ -89,6 +90,86 @@ public class AddTagToRelationshipCommandTest {
         assertEquals(String.format(AddTagToRelationshipCommand.MESSAGE_SUCCESS,
                 relationship.withAddedTag(newTag)), result.getFeedbackToUser());
         assertTrue(modelStub.isRelationshipUpdated);
+    }
+
+    @Test
+    public void equals() {
+        String userId1 = "user1";
+        String userId2 = "user2";
+        String relationshipName = "Friend";
+        Tag tag1 = new Tag("tag1");
+        Tag tag2 = new Tag("tag2");
+
+        AddTagToRelationshipCommand command1 = new AddTagToRelationshipCommand(userId1, userId2, relationshipName,
+                tag1);
+        AddTagToRelationshipCommand command1Copy = new AddTagToRelationshipCommand(userId1, userId2, relationshipName,
+                tag1);
+        AddTagToRelationshipCommand command2 = new AddTagToRelationshipCommand(userId1, userId2, relationshipName,
+                tag2);
+        AddTagToRelationshipCommand commandDifferentUser1 = new AddTagToRelationshipCommand("other", userId2,
+                relationshipName, tag1);
+        AddTagToRelationshipCommand commandDifferentUser2 = new AddTagToRelationshipCommand(userId1, "other",
+                relationshipName, tag1);
+        AddTagToRelationshipCommand commandDifferentName = new AddTagToRelationshipCommand(userId1, userId2, "Other",
+                tag1);
+
+        // same object -> returns true
+        assertEquals(command1, command1);
+
+        // same values -> returns true
+        assertEquals(command1, command1Copy);
+
+        // null -> returns false
+        assertNotEquals(null, command1);
+
+        // different tag -> returns false
+        assertNotEquals(command1, command2);
+
+        // different user ID 1 -> returns false
+        assertNotEquals(command1, commandDifferentUser1);
+
+        // different user ID 2 -> returns false
+        assertNotEquals(command1, commandDifferentUser2);
+
+        // different relationship name -> returns false
+        assertNotEquals(command1, commandDifferentName);
+    }
+
+    @Test
+    public void hashCode_test() {
+        String userId1 = "user1";
+        String userId2 = "user2";
+        String relationshipName = "Friend";
+        Tag tag1 = new Tag("tag1");
+        Tag tag2 = new Tag("tag2");
+
+        AddTagToRelationshipCommand command1 = new AddTagToRelationshipCommand(userId1, userId2, relationshipName,
+                tag1);
+        AddTagToRelationshipCommand command1Copy = new AddTagToRelationshipCommand(userId1, userId2, relationshipName,
+                tag1);
+        AddTagToRelationshipCommand command2 = new AddTagToRelationshipCommand(userId1, userId2, relationshipName,
+                tag2);
+
+        // Same values -> same hashcode
+        assertEquals(command1.hashCode(), command1Copy.hashCode());
+
+        // Different tag -> different hashcode
+        assertNotEquals(command1.hashCode(), command2.hashCode());
+
+        // Different user ID 1 -> different hashcode
+        AddTagToRelationshipCommand commandDifferentUser1 = new AddTagToRelationshipCommand("other", userId2,
+                relationshipName, tag1);
+        assertNotEquals(command1.hashCode(), commandDifferentUser1.hashCode());
+
+        // Different user ID 2 -> different hashcode
+        AddTagToRelationshipCommand commandDifferentUser2 = new AddTagToRelationshipCommand(userId1, "other",
+                relationshipName, tag1);
+        assertNotEquals(command1.hashCode(), commandDifferentUser2.hashCode());
+
+        // Different relationship name -> different hashcode
+        AddTagToRelationshipCommand commandDifferentName = new AddTagToRelationshipCommand(userId1, userId2, "Other",
+                tag1);
+        assertNotEquals(command1.hashCode(), commandDifferentName.hashCode());
     }
 
     private static class ModelStubWithoutRelationship extends ModelStub {
